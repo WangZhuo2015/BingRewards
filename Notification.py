806f90aa22fae66b79ac0a5c7f2080a68046c80f
王卓 <zhuowangwork@gmail.com>
2023-04-07 15:01:37 +0800

    add bark notification in Notification.py

---
+++ Notification.py
@@ -0,0 +1,22 @@
+import os
+from abc import abstractmethod
+
+import requests
+
+
+class Notification:
+    @abstractmethod
+    def notify(self, message):
+        pass
+
+
+class BarkNotification(Notification):
+    def __init__(self, api_key=os.environ.get("BARK_API_KEY")):
+        if api_key is None:
+            raise Exception("Bark API Key is not set")
+        self.api_key = api_key
+
+    def notify(self, message, title="Bing Rewards"):
+        # https://api.day.app/{api_key}}/Title/text
+        # send http get request to the url
+        requests.get(f"https://api.day.app/{self.api_key}/{title}/{message}")
